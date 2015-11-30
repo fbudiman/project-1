152b8773c8ce407805f9a2c8988fefc5e4d1e070
Fran Budiman <fbudiman14@hotmail.com>
2015-11-30 13:23:24 -0800

    completed intro page

--- js/javascript.js
+++ js/javascript.js
@@ -1,3 +1,17 @@
 $(function(){
 
+  $("#begin").hover(
+    function() {
+      $(this).animate({ color: "#700000" }, 'slow');
+    },function() {
+      $(this).animate({ color: "#FFFFFF" }, 'slow');
+  });
+
+  $("#intro").hide();
+
+  $("#begin").click(function() {
+      $(this).hide();
+      $("#intro").show();
+  });
+
 });
